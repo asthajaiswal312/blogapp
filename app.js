b4046b5cdc91ce70568cfc7b1e12a91fa3c90ab1
Astha <asthajaiswal312@gmail.com>
2024-06-05 23:58:04 +0530

    challenge 8 completed

--- app.js
+++ app.js
@@ -23,15 +23,30 @@ app.get("/", (req,res) => {
     homeStartingContent: homeStartingContent
   })
 })
+
+
 app.get("/about", (req,res) => {
   res.render("about", {
     aboutContent: aboutContent
   })
 })
+
+
 app.get("/contact", (req,res) => {
   res.render("contact", {
     contactContent: contactContent
   })
+})
+
+
+
+app.get("/compose", (req,res) => {
+  res.render("compose")
+})
+
+app.post("/compose", (req, res) => {
+  let post = req.body.post
+  console.log(post)
 })
 
 
